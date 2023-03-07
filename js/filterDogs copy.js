f995c030d70daf523428f1b65d05244306f8d923
Rous-99 <rous.bf.10@gmail.com>
2023-03-07 13:41:18 +0100

    filtro de gender, size y age terminados
    falta añadirle el reset para que se quite la opcion una vez clickada

--- js/filterDogs copy.js
+++ js/filterDogs copy.js
@@ -95,8 +95,43 @@ const showFilterDogs = (filterDogs) =>{
     })
 
 }
+
+const filterDogsByAge = async(age) =>{
+    console.log("desde la funcion",age);
+    let currentPage=1;
+    let filterDogs=[];
+    let dogJson=await getDogByAge(age, currentPage)
+    // console.log(dogsJson);
+    let numPag=dogsJson["pagination"].total_pages;
+    console.log(numPag);
+    for(let indPag=1;indPag<=numPag;indPag++){
+        let dogJson=await getDogByAge(age, indPag);
+        console.log(dogsJson);
+        filterDogs.push(dogJson["animals"]);
+    }
+    console.log(filterDogs);
+    showFilterDogs(filterDogs); 
+}
  
-const filterDogs= async (gender) => {
+
+const filterDogsBySize = async(size) =>{
+    console.log("desde la funcion",size);
+    let currentPage=1;
+    let filterDogs=[];
+    let dogJson=await getDogBySize(size, currentPage)
+    // console.log(dogsJson);
+    let numPag=dogsJson["pagination"].total_pages;
+    console.log(numPag);
+    for(let indPag=1;indPag<=numPag;indPag++){
+        let dogJson=await getDogBySize(size, indPag);
+        console.log(dogsJson);
+        filterDogs.push(dogJson["animals"]);
+    }
+    console.log(filterDogs);
+    showFilterDogs(filterDogs); 
+}
+ 
+const filterDogsByGender= async (gender) => {
     console.log("desde la funcion",gender);
     let currentPage=1;
     let filterDogs=[];
@@ -113,6 +148,41 @@ const filterDogs= async (gender) => {
     showFilterDogs(filterDogs);
 }
 
+const getDogByAge= async(age,currentPage) =>{
+    let newToken=await getToken();
+    // console.log("token del fetch",newToken);
+    const tokenType=tokenJson.token_type;
+    const tokenAcces=tokenJson.access_token;
+    const tokenExpires=tokenJson.expires_in;
+    console.log(tokenType,tokenAcces,tokenExpires);
+    const dogsReponse= await fetch(`https://api.petfinder.com/v2/animals?type=${animalType}&age=${age}&location=texas&distance=50&page=${currentPage}`,{ //devuelve un array de objetos
+     headers: {
+     'Authorization': tokenType + ' ' + tokenAcces,
+     'Content-Type': 'application/x-www/form-urlencoded'
+     }
+    }); //son 7 páginas y 133 perros en total
+
+    return dogsJson=await dogsReponse.json();
+}  
+
+
+const getDogBySize= async(size,currentPage) =>{
+    let newToken=await getToken();
+    // console.log("token del fetch",newToken);
+    const tokenType=tokenJson.token_type;
+    const tokenAcces=tokenJson.access_token;
+    const tokenExpires=tokenJson.expires_in;
+    console.log(tokenType,tokenAcces,tokenExpires);
+    const dogsReponse= await fetch(`https://api.petfinder.com/v2/animals?type=${animalType}&size=${size}&location=texas&distance=50&page=${currentPage}`,{ //devuelve un array de objetos
+     headers: {
+     'Authorization': tokenType + ' ' + tokenAcces,
+     'Content-Type': 'application/x-www/form-urlencoded'
+     }
+    }); //son 7 páginas y 133 perros en total
+
+    return dogsJson=await dogsReponse.json();
+}  
+
 const getDogByGender= async(gender,currentPage) =>{
     let newToken=await getToken();
     // console.log("token del fetch",newToken);
@@ -128,165 +198,25 @@ const getDogByGender= async(gender,currentPage) =>{
     }); //son 7 páginas y 133 perros en total
 
     return dogsJson=await dogsReponse.json();
-}
-    //  dogInfo.forEach(dog =>{
-    //     if(dog.photos.length>0){
-    //         let dogID=dog.url;
-    //         let photo=dog['primary_photo_cropped'].large;
-    //         let age=dog.age;
-    //         let breed=dog['breeds'].primary;
-    //         let gender=dog.gender;
-    //         let Name=dog.name;
-    //         let size=dog.size;
-    //         let description=dog["tags"];
-    //         console.log(dog);
-    //         console.log(dogID,photo,age,breed,gender,Name,description, size); 
-
-    //         output+=`
-    //         <div class="dog">
-    //             <img class="dog__photo" src="${photo}" alt="">
-    //             <div class="dog__info">
-    //                 <p class="name">${Name}</p>
-    //                 <div class="info__description">
-    //                     <p class="info__p">Age</p>
-    //                     <p class="lifespan">${age}</p>
-    //                 </div>
-    //                 <div class="info__description">
-    //                     <p class="info__p">Breed</p>
-    //                     <p class="temperament">${breed}</p>
-    //                 </div>
-    //                 <div class="info__description">
-    //                     <p class="info__p">Gender</p>
-    //                     <p class="height">${gender}</p>
-    //                 </div>
-    //                 <div class="info__description">
-    //                     <p class="info__p">Size</p>
-    //                     <p class="weight">${size}</p>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //         `
-    //         dogsContainer.innerHTML=output;
-    //     }   
-       
-    // });
-
-// const lifeSpanSelect = (dogs) => {
-//     console.log("dogs loaded");
-//     const lifespan=[];
-//     const lifeSpanSelect=document.querySelector('#lifespan__filter');
-//     console.log(lifeSpanSelect);
-//     const lifespanOptions=dogs.map( dog => {
-//         lifespan.push(dog.life_span);
-//     })
-//     console.log(lifespan);
-//     const lifespanFilter=[];
-//     //filtro las que se repiten
-//     lifespan.forEach((item)=>{
-//         if(!lifespanFilter.includes(item)){
-//             lifespanFilter.push(item);
-//         }
-//     })
-//     console.log(lifespanFilter);
-
-// }
-
-// const breedSelect=(dogs) =>{
-//     console.log("dog loaded", dogs);
-//     const breeds=[];
-//     const select=document.querySelector('#breed__filter');
-//     console.log(select);
-//     const dogsBreedsOptions=dogs.map(dog =>{
-//         breeds.push(dog.name);
-//         const breedOption=document.createElement('option');
-//         breedOption.textContent=dog.name;
-//         breedOption.value=dog.id; /*asi podemos acceder al objeto perro por la id */
-//         return breedOption;
-//     })
-//     console.log(dogsBreedsOptions);
-//     console.log("razas",breeds);
-//     dogsBreedsOptions.forEach(option => {
-//         select.appendChild(option);
-//     })
-   
-// }   
-
-// const setDog = async (imageUrl) => {
-//     // dogPhoto.setAttribute('src', imageUrl);
-//     const response=await fetch(baseAPIUrl + `/images/${imageUrl}`,requestOptions);
-//     const responseJson=await response.json();
-//     let dogPhotoById=responseJson.url;
-//     console.log(dogPhotoById);
-//     dogPhoto.setAttribute('src', dogPhotoById);
-// }
-
-// const setDescription = (temperament, lifespan, height, weight) =>{
-//     console.log(lifespan,weight,height,temperament);
-//     lifeSpanHTML.innerText=lifespan;
-//     temperamentHTML.innerText=temperament;
-//     heightHTML.innerText=height + " cm";
-//     weightHTML.innerText=weight + " kg";
-// }
+}  
 
 
-// const getDog= async (dogID) => {
-//     //hacemos unpacking del array
-//     const response = await fetch(baseAPIUrl+`/breeds/${dogID}`, requestOptions);
-//     const responseJson=await response.json();
-//     console.log(responseJson);
-    
-//     let dogLifespan=responseJson.life_span;
-//     let weight=responseJson["weight"].metric;
-//     let height=responseJson["height"].metric;
-//     let temperament=responseJson.temperament;
-//     let imageID=responseJson.reference_image_id;
-
-//     const getPhoto=await fetch(baseAPIUrl + `/images/${imageID}`,requestOptions);
-//     const getPhotJson=await getPhoto.json();
-//     let dogPhotoById=getPhotJson.url;
-//     console.log(dogLifespan, weight,height,temperament,imageID);
-//     console.log(dogsContainer.childNodes.length);
-//     if(dogsContainer.childNodes.length>1){
-//         let dog=document.querySelector('.dog');
-//         // console.log(dogsContainer.firstElementChild);
-//         console.log("soy el padre", dogsContainer);
-//         dogsContainer.removeChild(dogsContainer.firstElementChild); //no se elimina
-//     }
-//     output+=`
-//                             <div class="dog">
-//                                 <img class="dog__photo" src="${dogPhotoById}" alt="">
-//                                 <div class="dog__info">
-//                                     <div class="info__description">
-//                                         <p class="info__p">Lifespan</p>
-//                                         <p class="lifespan">${dogLifespan}</p>
-//                                     </div>
-//                                     <div class="info__description">
-//                                         <p class="info__p">Temperament</p>
-//                                         <p class="temperament">${temperament}</p>
-//                                     </div>
-//                                     <div class="info__description">
-//                                         <p class="info__p">Height</p>
-//                                         <p class="height">${height} cm</p>
-//                                     </div>
-//                                     <div class="info__description">
-//                                         <p class="info__p">Weight</p>
-//                                         <p class="weight">${weight} kg</p>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                             `
-//                         dogsContainer.innerHTML=output;
-// }
-
-
-const changeDog = () => {
+const changeDogByGender = () => {
     //AÑADIR ALGO QUE RESETEE EL CONTENEDOR DOGS PARA QUE NO SE AÑADAN DEBAJO LOS NUEVOS FILTROS
     console.log(event.target.value);
-    filterDogs(event.target.value);
+    filterDogsByGender(event.target.value);
     // dogsContainer.classList.remove('hide');//cuando cargue la primera selección ya no necesito esconderlo
-
 }
 
+const changeDogBySize = () => {
+    console.log(event.target.value);
+    filterDogsBySize(event.target.value);
+}
+
+const changeDogByAge = () => {
+    console.log(event.target.value);
+    filterDogsByAge(event.target.value);
+}
 //get the breeds when the page has loaded
 // document.addEventListener('DOMContentLoaded', async() => {
 //     await fetchDogs();
